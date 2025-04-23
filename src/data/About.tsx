bd8aa7f1cd6d7d74fcb98d92ff004789ccaf3335
dejesusbg <dejesusbg5@gmail.com>
2025-04-22 22:19:41 -0500

    feat: update bio for clarity and relevance

--- src/data/About.tsx
+++ src/data/About.tsx
@@ -1,11 +1,11 @@
 export const bio = `
-  I'm a full-stack developer passionate about building scalable, high-performance applications that seamlessly integrate frontend and backend technologies. I thrive at the intersection of software engineering and user experience, ensuring every project I work on is both functional and well-crafted.
+  I'm a developer passionate about building scalable, high-performance applications that seamlessly integrate frontend and backend technologies. I thrive at the intersection of software engineering and user experience, ensuring every project I work on is both functional and well-crafted.
 
-  Currently, I'm studying Systems Engineering at [University of Magdalena](https://www.unimagdalena.edu.co/) while gaining hands-on experience through freelance projects. I've developed solutions ranging from [corporate websites](https://rdingnova.com.co/) to AI-powered classification models, always prioritizing clean architecture, maintainability, and user-centric design.
+  Currently, I'm studying Systems Engineering at [University of Magdalena](https://www.unimagdalena.edu.co/) while actively working on freelance projects. I've developed software ranging from [AI-powered mango classifier](https://www.kaggle.com/datasets/adrinbd/unripe-ripe-rotten-mango) to [navigation app for the visually impaired](https://github.com/dejesusbg/truenavi), showcasing my ability to develop impactful, real-world applications. 
 
-  Throughout my journey, I've worked with technologies like React, Next.js, Django, and Spring Boot, crafting digital experiences that enhance business operations and user engagement. Whether it's optimizing an [AI model for mango classification](https://www.kaggle.com/datasets/adrinbd/unripe-ripe-rotten-mango) or developing an [offline-first habit-tracking app](https://auraby.netlify.app/), I'm always driven by the challenge of creating impactful solutions.
+  I'm proficient in technologies like React, TypeScript, Node.js, Python, TensorFlow, and Docker, focusing on clean architecture and maintainable code. Whether it's building a [modern corporate site](https://rdingnova.com.co/) or an [offline-first habit tracker](https://auraby.netlify.app/), I am passionate about crafting solutions that enhance user engagement and business operations.
 
   In my spare time, I'm usually listening to 
   <span id="song">[The Marías](https://open.spotify.com/track/3siwsiaEoU4Kuuc9WKMUy5)</span>,
-  exercising, or exploring emerging tech to refine my coding skills and plan my next [software project](/resume.pdf).
+  exercising, or diving into emerging tech to stay ahead of the curve and fuel my next [software project](/resume.pdf).
 `;
