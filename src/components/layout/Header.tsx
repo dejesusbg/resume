f2535cdd8477aed63251dad6137afe9872e4929d
dejesusbg <dejesusbg5@gmail.com>
2025-05-16 23:18:11 -0500

    feat: refactor and improve navigation transition

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,119 +1,104 @@
-"use client";
-import { useEffect, useState } from "react";
-import clsx from "clsx";
+'use client';
 import {
-  NavigationItemProps,
-  navigationItems,
-  SocialLinkProps,
-  socialLinks,
-  info,
-} from "@/data/Header";
+	info,
+	NavigationItemProps,
+	navigationItems,
+	SocialLinkProps,
+	socialLinks,
+} from '@/data/Header';
+import clsx from 'clsx';
+import { useEffect, useState } from 'react';
 
-const NavigationItem = ({
-  item,
-  isActive = false,
-  onClick,
-}: NavigationItemProps) => (
-  <a
-    href={`#${item}`}
-    className="flex items-center group w-min px-1"
-    onClick={onClick}
-  >
-    <span
-      className={clsx(
-        "mr-2 text-lovie w-2.5 select-none font-medium transition-all duration-300",
-        {
-          "text-frost scale-125 opacity-100": isActive,
-          "scale-100 opacity-60": !isActive,
-        }
-      )}
-    >
-      {isActive ? ">" : "_"}
-    </span>
-    <span
-      className={clsx(
-        "font-normal text-lg tracking-tight grocus:font-bold grocus:tracking-tighter grocus:text-frost",
-        { "text-frost !font-bold !tracking-tighter": isActive }
-      )}
-    >
-      {item}
-    </span>
-  </a>
+const NavigationItem = ({ item, isActive = false, onClick }: NavigationItemProps) => (
+	<a
+		href={`#${item}`}
+		className={clsx(
+			'block w-min font-normal text-lg tracking-tight transition-all duration-300 hocus:font-bold hocus:tracking-tighter hocus:text-frost',
+			{ 'translate-x-0 text-frost !font-bold !tracking-tighter': isActive }
+		)}
+		onClick={() => setTimeout(onClick, 300)}>
+		{item}
+	</a>
 );
 
 const SocialLink = ({ icon: Icon, link, size }: SocialLinkProps) => (
-  <a
-    href={link}
-    className="hover:text-frost"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    <Icon size={size} />
-  </a>
+	<a href={link} className="hover:text-frost" target="_blank" rel="noopener noreferrer">
+		<Icon size={size} />
+	</a>
 );
 
 const Navigation = () => {
-  const [activeItem, setActiveItem] = useState("about");
-  const sections = navigationItems;
+	const [activeIndex, setActiveIndex] = useState(0);
+	const sections = navigationItems;
 
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) setActiveItem(entry.target.id);
-        });
-      },
-      { threshold: 0.5 }
-    );
+	useEffect(() => {
+		const observer = new IntersectionObserver(
+			(entries) => {
+				entries.forEach((entry) => {
+					if (entry.isIntersecting) setActiveIndex(sections.indexOf(entry.target.id));
+				});
+			},
+			{ threshold: 0.5 }
+		);
 
-    sections.forEach((section) => {
-      const element = document.getElementById(section);
-      if (element) observer.observe(element);
-    });
+		sections.forEach((section, index) => {
+			const element = document.getElementById(section);
+			if (element) {
+				observer.observe(element);
+				if (index === sections.indexOf(section)) setActiveIndex(index);
+			}
+		});
 
-    return () => observer.disconnect();
-  }, [sections]);
+		return () => observer.disconnect();
+	}, [sections]);
 
-  return (
-    <nav className="mt-16 hidden lg:block space-y-2">
-      {sections.map((name) => (
-        <NavigationItem
-          key={name}
-          item={name}
-          isActive={activeItem === name}
-          onClick={() => setActiveItem(name)}
-        />
-      ))}
-    </nav>
-  );
+	return (
+		<nav className="relative hidden gap-3 mt-16 select-none lg:flex">
+			<span
+				className="text-xl font-semibold transition-all duration-100 ease-in-out h-9 text-lovie"
+				style={{ transform: `translateY(${activeIndex * 100}%)` }}>
+				{'>'}
+			</span>
+			<div className="space-y-2">
+				{sections.map((name, index) => (
+					<NavigationItem
+						key={name}
+						item={name}
+						isActive={activeIndex === index}
+						onClick={() => setActiveIndex(index)}
+					/>
+				))}
+			</div>
+		</nav>
+	);
 };
 
 const SocialLinks = () => {
-  return (
-    <div className="px-1 flex space-x-5 mt-8 items-center">
-      {socialLinks.map((link) => (
-        <SocialLink key={link.link} {...link} />
-      ))}
-    </div>
-  );
+	return (
+		<div className="flex items-center px-1 mt-8 space-x-5">
+			{socialLinks.map((link) => (
+				<SocialLink key={link.link} {...link} />
+			))}
+		</div>
+	);
 };
 
 const Header = () => {
-  return (
-    <header className="flex flex-col justify-between lg:top-0 w-full lg:w-[48%] lg:max-h-screen lg:sticky lg:py-24">
-      <div>
-        <h1 className="text-4xl font-bold tracking-tight sm:text-5xl">
-          <a href="https://dejesusbg.netlify.app">{info.name}</a>
-        </h1>
-        <h2 className="mt-3 text-lg sm:text-xl font-medium">{info.title}</h2>
-        <p className="leading-normal mt-4 max-w-xs">{info.description}</p>
-        <Navigation />
-      </div>
-      <div>
-        <SocialLinks />
-      </div>
-    </header>
-  );
+	return (
+		<header className="flex flex-col justify-between lg:top-0 w-full lg:w-[48%] lg:max-h-screen lg:sticky lg:py-24">
+			<div>
+				<h1 className="text-4xl font-bold tracking-tight sm:text-5xl">
+					<a href="https://dejesusbg.netlify.app">{info.name}</a>
+				</h1>
+				<h2 className="mt-3 text-lg font-medium sm:text-xl">{info.title}</h2>
+				<p className="max-w-xs mt-4 leading-normal">{info.description}</p>
+				<Navigation />
+			</div>
+			<div>
+				<SocialLinks />
+			</div>
+		</header>
+	);
 };
 
 export default Header;
--- src/data/Header.tsx
+++ src/data/Header.tsx
@@ -1,38 +1,28 @@
-import { IconType } from "react-icons";
-import {
-  FaEnvelope,
-  FaGithub,
-  FaInstagram,
-  FaLinkedin,
-  FaSpotify,
-} from "react-icons/fa";
+import { IconType } from 'react-icons';
+import { FaGithub, FaLinkedin, FaSpotify } from 'react-icons/fa';
 
 export interface NavigationItemProps {
-  item: string;
-  isActive?: boolean;
-  onClick?: () => void;
-}
-export interface SocialLinkProps {
-  icon: IconType;
-  link: string;
-  size: number;
+	item: string;
+	isActive?: boolean;
+	onClick: () => void;
 }
 
-export const navigationItems = ["about", "projects", "gallery"];
+export interface SocialLinkProps {
+	icon: IconType;
+	link: string;
+	size: number;
+}
+
+export const navigationItems = ['about', 'projects', 'gallery'];
 
 export const socialLinks = [
-  { icon: FaGithub, link: "https://github.com/dejesusbg", size: 24 },
-  { icon: FaLinkedin, link: "https://linkedin.com/in/dejesusbg", size: 22 },
-  {
-    icon: FaSpotify,
-    link: "https://open.spotify.com/user/31zwlj7mwcvu3w2oklmyhna4s62a?si=46e92cc8ce5f49ac",
-    size: 22,
-  },
-  { icon: FaInstagram, link: "https://www.instagram.com/dejesusbg", size: 25 },
+	{ icon: FaGithub, link: 'https://github.com/dejesusbg', size: 24 },
+	{ icon: FaLinkedin, link: 'https://linkedin.com/in/dejesusbg', size: 22 },
+	{ icon: FaSpotify, link: 'https://open.spotify.com/user/31zwlj7mwcvu3w2oklmyhna4s62a', size: 22 },
 ];
 
 export const info = {
-  name: "Ricardo Barrios",
-  title: "Full Stack Engineer",
-  description: "I love to build experiences that help others.",
+	name: 'Ricardo Barrios',
+	title: 'Full Stack Engineer',
+	description: 'I love to build experiences that help others.',
 };
