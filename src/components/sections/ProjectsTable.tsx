247abb76075c8cfc4db3f2dfba44cfe015c6d140
dejesusbg <dejesusbg5@gmail.com>
2025-05-16 23:19:29 -0500

    feat: refactor and add projects description

--- src/components/sections/ProjectsTable.tsx
+++ src/components/sections/ProjectsTable.tsx
@@ -1,40 +1,36 @@
-import { projects } from "@/data/Projects";
-import ProjectsTableRow from "@/components/ui/ProjectsRow";
+import ProjectsTableRow from '@/components/ui/ProjectsRow';
+import { projects } from '@/data/Projects';
 
 const ProjectsTable = () => {
-  const sortedProjects = [...projects].sort((a, b) => {
-    const [monthA, yearA] = a.date.split("/");
-    const [monthB, yearB] = b.date.split("/");
+	const sortedProjects = [...projects].sort((a, b) => {
+		const [monthA, yearA] = a.date.split('/');
+		const [monthB, yearB] = b.date.split('/');
 
-    const dateA = `20${yearA}-${monthA.padStart(2, "0")}`;
-    const dateB = `20${yearB}-${monthB.padStart(2, "0")}`;
+		const dateA = `20${yearA}-${monthA.padStart(2, '0')}`;
+		const dateB = `20${yearB}-${monthB.padStart(2, '0')}`;
 
-    return new Date(dateB).getTime() - new Date(dateA).getTime();
-  });
+		return new Date(dateB).getTime() - new Date(dateA).getTime();
+	});
 
-  return (
-    <table className="mt-12 w-full border-collapse text-left">
-      <thead className="sticky top-0 z-10 border-b border-cloud/20 px-6 py-5 backdrop-blur bg-ebony/75">
-        <tr>
-          <th className="py-4 pr-8 text-sm font-semibold text-frost">Date</th>
-          <th className="py-4 pr-8 text-sm font-semibold text-frost">
-            Project
-          </th>
-          <th className="hidden py-4 px-8 text-sm font-semibold text-frost lg:table-cell">
-            Built with
-          </th>
-          <th className="hidden py-4 pr-8 text-sm font-semibold text-frost sm:table-cell">
-            Link
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {sortedProjects.map((project, index) => (
-          <ProjectsTableRow key={index} {...project} />
-        ))}
-      </tbody>
-    </table>
-  );
+	return (
+		<table className="w-full mt-12 text-left border-collapse">
+			<thead className="sticky top-0 z-10 px-6 py-5 border-b border-cloud/20 backdrop-blur bg-ebony/75">
+				<tr>
+					<th className="py-4 pr-8 text-sm font-semibold text-frost">Year</th>
+					<th className="py-4 pr-8 text-sm font-semibold text-frost">Project</th>
+					<th className="hidden py-4 pr-8 text-sm font-semibold text-frost lg:table-cell">
+						Built with
+					</th>
+					<th className="hidden py-4 pr-8 text-sm font-semibold text-frost sm:table-cell">Link</th>
+				</tr>
+			</thead>
+			<tbody>
+				{sortedProjects.map((project, index) => (
+					<ProjectsTableRow key={index} {...project} />
+				))}
+			</tbody>
+		</table>
+	);
 };
 
 export default ProjectsTable;
--- src/components/ui/ProjectsRow.tsx
+++ src/components/ui/ProjectsRow.tsx
@@ -1,115 +1,110 @@
-import { ProjectProps } from "@/data/Projects";
-import { ProjectsTableTag } from "./ProjectsTag";
-import { FaGithub } from "react-icons/fa";
+import { ProjectProps } from '@/data/Projects';
+import { FaGithub } from 'react-icons/fa';
+import { ProjectsTableTag } from './ProjectsTag';
 
-const ProjectsTableRow = ({ title, link, tags, date }: ProjectProps) => {
-  const isLink = link !== "#";
+const ProjectsTableRow = ({ title, description, link, tags, date }: ProjectProps) => {
+	const isLink = link !== '#';
 
-  const isGithub = isLink && link.includes("github.com");
-  const githubRepoName = isGithub
-    ? link
-        .replace(/^https?:\/\/github\.com\//, "")
-        .replace(/\/$/, "")
-        .replace("dejesusbg/", "")
-    : null;
+	const isGithub = isLink && link.includes('github.com');
+	const githubRepoName = isGithub
+		? link
+				.replace(/^https?:\/\/github\.com\//, '')
+				.replace(/\/$/, '')
+				.replace('dejesusbg/', '')
+		: null;
 
-  const linkDisplay = isGithub
-    ? githubRepoName
-    : isLink
-    ? link.replace(/^https?:\/\//, "").replace(/\/$/, "")
-    : link;
+	const linkDisplay = isGithub
+		? githubRepoName
+		: isLink
+		? link.replace(/^https?:\/\//, '').replace(/\/$/, '')
+		: link;
 
-  const yearDisplay = "20" + date.split("/")[1];
+	const yearDisplay = '20' + date.split('/')[1];
 
-  return (
-    <tr className="border-b border-cloud/20 last:border-none">
-      <td className="py-4 pr-4 align-top text-sm">
-        <div className="translate-y-px text-frost/60">{yearDisplay}</div>
-      </td>
-      <td className="py-4 pr-4 align-top font-semibold leading-snug text-frost">
-        <div>
-          <div className="block sm:hidden">
-            {isLink ? (
-              <a
-                className="inline-flex items-baseline font-medium leading-tight hocus:text-lovie sm:hidden group/link text-base"
-                href={link}
-                target="_blank"
-                rel="noreferrer noopener"
-                aria-label={title}
-              >
-                <span>
-                  <span className="inline-block">
-                    {title}
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                      className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
-                      aria-hidden="true"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
-                        clipRule="evenodd"
-                      ></path>
-                    </svg>
-                  </span>
-                </span>
-              </a>
-            ) : (
-              title
-            )}
-          </div>
-          <div className="hidden sm:block">{title}</div>
-        </div>
-      </td>
-      <td className="hidden py-4 px-8 align-top lg:table-cell">
-        <ul className="flex -translate-y-1.5 flex-wrap">
-          {tags.map((tag, index) => (
-            <ProjectsTableTag key={index} name={tag} />
-          ))}
-        </ul>
-      </td>
-      <td className="hidden py-4 align-top sm:table-cell">
-        <ul className="translate-y-1">
-          <li className="mb-1 flex items-center">
-            {isLink && (
-              <a
-                className="inline-flex items-baseline font-medium leading-tight text-frost/60 hocus:text-lovie text-sm group/link text-sm"
-                href={link}
-                target="_blank"
-                rel="noreferrer noopener"
-                aria-label={title}
-              >
-                <span>
-                  <span className="inline-block">
-                    {linkDisplay}
-                    {isGithub ? (
-                      <FaGithub className="inline-block ml-1" size={14} />
-                    ) : (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-0.5"
-                        aria-hidden="true"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
-                          clipRule="evenodd"
-                        ></path>
-                      </svg>
-                    )}
-                  </span>
-                </span>
-              </a>
-            )}
-          </li>
-        </ul>
-      </td>
-    </tr>
-  );
+	return (
+		<tr className="border-b border-cloud/20 last:border-none">
+			<td className="py-4 pr-4 text-sm align-top">
+				<div className="translate-y-px text-frost/60">{yearDisplay}</div>
+			</td>
+			<td className="py-4 pr-8 font-semibold leading-snug align-top">
+				<div>
+					<div className="block sm:hidden">
+						{isLink ? (
+							<a
+								className="inline-flex items-baseline text-base font-medium leading-tight hocus:text-lovie sm:hidden group/link"
+								href={link}
+								target="_blank"
+								rel="noreferrer noopener"
+								aria-label={title}>
+								<span>
+									<span className="inline-block text-frost">
+										{title}
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											viewBox="0 0 20 20"
+											fill="currentColor"
+											className="inline-block w-4 h-4 ml-1 transition-transform translate-y-px shrink-0 group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none"
+											aria-hidden="true">
+											<path
+												fillRule="evenodd"
+												d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+												clipRule="evenodd"></path>
+										</svg>
+									</span>
+								</span>
+							</a>
+						) : (
+							<span className="text-frost">{title}</span>
+						)}
+					</div>
+					<div className="hidden sm:block text-frost">{title}</div>
+				</div>
+				<p className="block mt-2 text-sm font-normal leading-normal sm:max-w-md">{description}</p>
+			</td>
+			<td className="hidden py-4 pr-4 align-top lg:table-cell">
+				<ul className="flex -translate-y-1.5 flex-wrap">
+					{tags.map((tag, index) => (
+						<ProjectsTableTag key={index} name={tag} />
+					))}
+				</ul>
+			</td>
+			<td className="hidden py-4 align-top sm:table-cell">
+				<ul className="translate-y-1">
+					<li className="flex items-center mb-1 text-nowrap">
+						{isLink && (
+							<a
+								className="inline-flex items-baseline text-sm font-medium leading-tight text-frost/60 hocus:text-lovie group/link"
+								href={link}
+								target="_blank"
+								rel="noreferrer noopener"
+								aria-label={title}>
+								<span>
+									<span className="inline-block">
+										{linkDisplay}
+										{isGithub ? (
+											<FaGithub className="inline-block ml-1.5" size={14} />
+										) : (
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												viewBox="0 0 20 20"
+												fill="currentColor"
+												className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-0.5"
+												aria-hidden="true">
+												<path
+													fillRule="evenodd"
+													d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+													clipRule="evenodd"></path>
+											</svg>
+										)}
+									</span>
+								</span>
+							</a>
+						)}
+					</li>
+				</ul>
+			</td>
+		</tr>
+	);
 };
 
 export default ProjectsTableRow;
