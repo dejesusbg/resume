4920de02399615eba22b208e94fe2f94bf79f58a
dejesusbg <dejesusbg5@gmail.com>
2025-05-26 22:50:52 -0500

    fix: clarify description in bio

--- src/data/About.tsx
+++ src/data/About.tsx
@@ -3,7 +3,7 @@ export const bio = `
 
   Currently, I'm studying Systems Engineering at [University of Magdalena](https://www.unimagdalena.edu.co/) while working on freelance, academic, and personal software projects ranging from an [AI-powered mango classifier](https://www.kaggle.com/datasets/adrinbd/unripe-ripe-rotten-mango) to a [navigation app for the visually impaired](https://github.com/dejesusbg/truenavi), showcasing my ability to create impactful real-world applications.
 
-  I'm proficient in technologies like React, TypeScript, Node.js, Python, TensorFlow, and Docker, focusing on clean architecture and maintainable code. Whether it's building a [modern corporate site](https://rdingnova.com.co/) or a [conversation analysis tool](https://github.com/dejesusbg/convolens) leveraging NLP and deep learning, I enjoy crafting solutions that enhance user engagement and business value.
+  I'm proficient in technologies like React, TypeScript, Node.js, Python, TensorFlow, and Docker, focusing on clean architecture and maintainable code. Whether it's building a [modern corporate site](https://rdingnova.com.co/) or a [conversation analysis tool](https://github.com/dejesusbg/convolens) leveraging natural language processing and deep learning, I enjoy crafting solutions that enhance user engagement and actual value.
 
   In my spare time, I'm usually listening to <span id="song">[The Marías](https://open.spotify.com/track/3siwsiaEoU4Kuuc9WKMUy5)</span>, exercising, or diving into emerging tech to stay ahead of the curve and fuel my next [software project](/resume.pdf).
 `;
