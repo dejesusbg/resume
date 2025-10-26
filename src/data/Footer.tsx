c1fe10e8f1bc525047e212bf056cc92f5b3275fe
dejesusbg <dejesusbg5@gmail.com>
2025-10-26 01:58:00 -0500

    update credits text

--- src/data/Footer.tsx
+++ src/data/Footer.tsx
@@ -1,3 +1,3 @@
 export const credits = `
-Loosely designed in [Figma](https://www.figma.com/) and coded in [VS Code](https://code.visualstudio.com/) by yours truly. Built with [Next.js](https://nextjs.org/) and [Tailwind CSS](https://tailwindcss.com/), deployed with [Netlify](https://www.netlify.com/). All text is set in the [Inter](https://rsms.me/inter/) and [Instrument](https://fonts.adobe.com/fonts/instrument-serif) typefaces.
+Loosely sketched on [Paper](https://en.wikipedia.org/wiki/Paper) and jumped into [VS Code](https://code.visualstudio.com/) to make it real. Built with [Next.js](https://nextjs.org/), styled with [Tailwind CSS](https://tailwindcss.com/), and deployed on [Netlify](https://www.netlify.com/). All text is set in the [Inter](https://rsms.me/inter/) and [Instrument](https://fonts.adobe.com/fonts/instrument-serif) typefaces.
 `;
