47af3f5d917bc9a360e2c4a90a59b6abca91b4ba
dejesusbg <dejesusbg5@gmail.com>
2025-10-28 00:14:23 -0500

    update languages (+french)

--- src/i18n/locale.ts
+++ src/i18n/locale.ts
@@ -1,6 +1,6 @@
 import { ProjectProps } from '@/components/ui/Project';
 
-export const locales = ['en', 'es'];
+export const locales = ['en', 'es', 'fr'];
 export const defaultLocale = 'en';
 
 export interface Messages {
